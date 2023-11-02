967b859ecc351f61d6bc2e7dd12575051102871d
Brandon Rozek <brozek@brandonrozek.com>
2023-11-02 16:54:40 -0400

    Matching the uninformed heuristic default

--- src/main/java/org/rairlab/planner/heuristics/ConstantHeuristic.java
+++ src/main/java/org/rairlab/planner/heuristics/ConstantHeuristic.java
@@ -3,7 +3,8 @@ package org.rairlab.planner.heuristics;
 import org.rairlab.planner.State;
 
 public class ConstantHeuristic {
+    // Uninformed heuristic
     public static int h(State s) {
-        return 1;
+        return 0;
     }
 }
